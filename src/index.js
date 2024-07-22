b972fd6e97ed7f6c192d51d1578598f4901c7cac
luoxiao <1025534573@qq.com>
2024-07-22 18:02:44 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("fast-cache")
+class FastCache {
+  constructor() {
+    this.state = {}
+  }
+  set(key, value) {
+    this.state[key] = value
+
+  }
+  get(key) {
+    return this.state[key]  
+  }
+}
+
+window.FastCache = FastCache
